b6d841e1f7af4808dc0a2198d9301492cf619b39
Sergey Semushin <predelnik@gmail.com>
2018-03-30 08:08:54 +0300

    Refactor things around

---
+++ Context.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "../nlohmann/json.hpp"
+#include "GameConfig.h"
+
+#include "Point.h"
+
+using nlohmann::json;
+
+class MyPart;
+class Food;
+class Ejection;
+class Player;
+class Virus;
+
+class Context {
+public:
+  Context ();
+  ~Context ();
+  void update_config(const json &data);
+  void update(const json &data);
+
+private:
+  void fill_objects(const json &data);
+  void update_caches ();
+  void update_my_center();
+
+public:
+  GameConfig config;
+  std::vector<MyPart> my_parts;
+  std::vector<Food> food;
+  std::vector<Ejection> ejections;
+  std::vector<Player> players;
+  std::vector<Virus> viruses;
+
+  Point my_center;
+};
