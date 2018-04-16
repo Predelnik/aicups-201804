1264da411e744a6e9075b23ba68a3b23fb64c897
Sergey Semushin <predelnik@gmail.com>
2018-04-16 07:42:37 +0300

    Optimize enemy prediction speed. Mind possible fusion mass for splitting.

--- MaxSpeedStrategy.h
+++ MaxSpeedStrategy.h
@@ -3,6 +3,7 @@
 #include "Strategy.h"
 
 #include "Defines.h"
+#include "KnownPlayer.h"
 #include "Point.h"
 #include <deque>
 #include <random>
@@ -16,19 +17,25 @@ struct FoodSeen {
 };
 
 class MaxSpeedStrategy : public Strategy {
+  constexpr static int food_shelf_life = 350;
+  constexpr static int angle_partition_count = DEBUG_RELEASE_VALUE(20, 40);
+  constexpr static int future_scan_iteration_count =
+      DEBUG_RELEASE_VALUE(10, 20);
+
 public:
   MaxSpeedStrategy();
-    Point border_point_by_vector(const Point& v);
-    Response get_response(const Context &context) override;
+  Point border_point_by_vector(const Point &v);
+  Response get_response(const Context &context) override;
   void initialize(const GameConfig &config) override;
 
 private:
   Response move_by_vector(const Point &v);
-  double calc_target_score(const Point& target);
+  void calculate_fusions(const std::vector<KnownPlayer> &enemies);
+  int get_scan_precision() const;
+  double calc_target_score(const Point &target);
   bool is_splitting_dangerous() const;
+  void calculate_predicted_enemies();
   Response get_response_impl();
-  Response speed_case();
-  Response no_speed_case();
   void remove_eaten_food();
   void remove_stale_food();
   void add_new_food_to_seen();
@@ -37,15 +44,13 @@ private:
 private:
   std::deque<FoodSeen> m_food_seen;
   std::multiset<Point> m_food_seen_set;
+  std::vector<KnownPlayer> m_fusions;
+  std::array<std::vector<KnownPlayer>, future_scan_iteration_count>
+      m_predicted_enemies;
 #ifdef CUSTOM_DEBUG
   std::vector<std::array<Point, 2>> m_debug_lines;
   std::vector<std::string> m_debug_line_colors;
 #endif;
-
-  constexpr static int food_shelf_life = 350;
-  constexpr static int angle_partition_count = DEBUG_RELEASE_VALUE(20, 40);
-  constexpr static int future_scan_iteration_count =
-      DEBUG_RELEASE_VALUE(10, 20);
   const Context *ctx;
   mutable std::default_random_engine m_re;
 };
